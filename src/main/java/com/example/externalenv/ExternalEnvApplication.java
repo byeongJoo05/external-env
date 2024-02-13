63bf4fc3ae27587d96f611c64352682495314a82
SonByeongJoo <thsqudwn05@gmail.com>
2024-02-13 21:26:10 +0900

    initial commit

---
+++ src/main/java/com/example/externalenv/ExternalEnvApplication.java
@@ -0,0 +1,13 @@
+package com.example.externalenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalEnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalEnvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/externalenv/ExternalEnvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.externalenv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalEnvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
